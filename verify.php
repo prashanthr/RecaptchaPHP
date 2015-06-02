2e609802c80f7f86ce3341450da873e64747f2b6
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2015-06-02 16:06:57 -0500

    Adding structure & updating recaptcha verification code
    Creating verify function and using constants from constants.php

--- verify.php
+++ verify.php
@@ -2,25 +2,40 @@
 /* verify.php */
 /* https://github.com/prashanthr/RecaptchaPHP - Prashanth Rajaram */
 include("constants.php");
-$previousPage = $_SERVER['HTTP_REFERER'];
-$iPAddress = $_SERVER['REMOTE_ADDR'];
+$previousPageUri = $_SERVER['HTTP_REFERER'];
+$successPageUri = $_SERVER['SERVER_NAME'];
 if(isset($_POST['g-recaptcha-response']))
 {
-  $reCaptchaResponse = $_POST['g-recaptcha-response'];
+  $recaptchaResponse = $_POST['g-recaptcha-response'];
+  $isVerified = VerifyRecaptchaResponse($recaptchaResponse);
+  if(isVerified)
+  {
+    //Re-captcha solved correctly. Proceed...
+    header('Location: '.$successPageUri);
+    die();
+  }
+  else
+  {
+    //Re-captcha incorrect. Spammer alert!
+    header('Location: '.$previousPageUri);
+    die();
+  }
 }
 else
 {
   //Re-Captcha not solved
+  
 }
 
-$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret="."[YOUR SECRET KEY]"."&response=".$reCaptchaResponse."&remoteip=".$iPAddress);
-if($response.success==false)
+function VerifyRecaptchaResponse($recaptchaResponse)
 {
-  echo "Re-captcha incorrect. You are spammer!";
+  $iPAddress = $_SERVER['REMOTE_ADDR'];
+  $recaptchaVerificationUri = Constants::RECAPTCHA_VERIFY_URI."?secret=".Constants::RECAPTCHA_SECRET_KEY."&response=".$recaptchaResponse."&remoteip=".$iPAddress);
+  $recaptchaResponse=file_get_contents($recaptchaVerificationUri);
+  if($recaptchaResponse.success==false)
+  {
+    return true;
+  }
+  return false;
 }
-else
-{
-  echo "Re-captcha solved correctly.";
-}
-
 ?>
