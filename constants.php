28c13aafcd7d260508b77b5d08db892f037c2205
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2015-06-02 12:15:31 -0500

    Converting global constants to class constants

--- constants.php
+++ constants.php
@@ -1,8 +1,10 @@
 <?php
 /* constants.php */
 /* github.com/prashanthr/RecaptchaPHP - Prashanth Rajaram */
-
-define("RECAPTCHA_SECRET_KEY", "[YOUR_SECRET_KEY]");
-define("RECAPTCHA_SITE_KEY", "[YOUR_SITE_KEY]");
-define("RECAPTCHA_VERIFY_URI", "https://www.google.com/recaptcha/api/siteverify");
+class Constants 
+{
+  const RECAPTCHA_SECRET_KEY = "[YOUR_SECRET_KEY]";
+  const RECAPTCHA_SITE_KEY = "[YOUR_SITE_KEY]";
+  const RECAPTCHA_VERIFY_URI = "https://www.google.com/recaptcha/api/siteverify";
+}
 ?>
