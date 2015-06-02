ee6b440cec7d1f90c17a7e2d59c1c2995baec466
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2015-06-02 11:28:56 -0500

    Creating constants.php
    Constants.php houses all constants required for ReCaptcha to work

---
+++ constants.php
@@ -0,0 +1,8 @@
+<?php
+/* constants.php */
+/* github.com/prashanthr/RecaptchaPHP - Prashanth Rajaram */
+
+define("RECAPTCHA_SECRET_KEY", "[YOUR_SECRET_KEY]");
+define("RECAPTCHA_SITE_KEY", "[YOUR_SITE_KEY]");
+define("RECAPTCHA_VERIFY_URI", "https://www.google.com/recaptcha/api/siteverify");
+?>
