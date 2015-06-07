2d0b4acb64eb2d102772696fd10d4c8d83f6c968
Prashanth Rajaram <prashweb@gmail.com>
2015-06-07 17:17:20 -0500

    Adding metadata to file

--- src/recaptchaApi.js
+++ src/recaptchaApi.js
@@ -1,3 +1,6 @@
+/* recaptchaApi.js */
+/* github.com/prashanthr/RecaptchaPHP - Prashanth Rajaram */
+
 var reCaptchaUri = "https://www.google.com/recaptcha/api.js"
 $.getScript(reCaptchaUri, function(){
    // Google ReCaptcha API loaded
